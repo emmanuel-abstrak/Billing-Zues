0e23bf1796be8cc511d9f4109f4ba870a2bb60a6
Emmanuel <emmanuel@abstrak.agency>
2024-10-22 21:01:08 +0200

    Save

--- app/Http/Resources/BillingProviderResource.php
+++ app/Http/Resources/BillingProviderResource.php
@@ -14,6 +14,7 @@ class BillingProviderResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'endpoint' => $this->endpoint,
             'active' => $this->active
         ];
     }
